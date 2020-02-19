90b17593b58ca9b64e2f69430c0f63f5b4442ee0
Michael Schappacher <michael.schappacher@stelligent.com>
2020-02-19 17:33:30 -0500

    Patch: Fix issue with rule not sending message to SNS. Fixed and simplified logic.

--- source/ec2_ami_not_encrypted.py
+++ source/ec2_ami_not_encrypted.py
@@ -9,10 +9,9 @@ class Ec2AmiNotEncrypted(AWSRule):
     """ AWS rule for ensuring S3 bucket encryption """
 
     def __init__(self, event):
-        super().__init__(event)
-        self.is_compliant = False
         self.raw_event = None
         self.ami_block_device_mapping = None
+        super().__init__(event)
 
     def extract_event_data(self, event):
         """ To be implemented by every rule """
@@ -25,8 +24,7 @@ class Ec2AmiNotEncrypted(AWSRule):
 
     def resource_compliant(self):
         """ True if all blocks are set to True."""
-        self.is_compliant = self.all_block_devices_encrypted()
-        return self.is_compliant
+        return self.all_block_devices_encrypted()
 
     def all_block_devices_encrypted(self):
         """Iterates over blocks and checks if True."""
@@ -36,11 +34,8 @@ class Ec2AmiNotEncrypted(AWSRule):
         return True
 
     def get_remediation_message(self):
-        if self.is_compliant is True:
-            return "All AMI EC2 block devices are currently encrypted"
-        else:
-            return f"EC2 AMI block devices are not" \
-                   f" currently encrypted, {self.raw_event}"
+        return f"EC2 AMI block devices are not" \
+               f" currently encrypted, {self.raw_event}"
 
     def remediate(self):
         self.resource_compliant()

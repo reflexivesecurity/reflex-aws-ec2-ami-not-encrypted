ce130fdc65b43a4b70dc687220df0ff2d6373ac4
Chris Coleman <chris@rot-26.com>
2020-04-12 15:30:47 -0600

    Renamed tf: from `detect_ec2_ami_not_encrypted` to `ec2_ami_not_encrypted`

--- ec2_ami_not_encrypted.tf
+++ ec2_ami_not_encrypted.tf
@@ -1,4 +1,4 @@
-module "detect_ec2_ami_not_encrypted" {
+module "ec2_ami_not_encrypted" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
   rule_name        = "DetectEc2AmiNotEncrypted"
   rule_description = "A Reflex rule for detecting the creation of unencrypted EC2 AMIs."
